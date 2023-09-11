bd15962051b03e187fa8e838cd2fbbe023fa90db
d2-pc <144701839+d2-pc@users.noreply.github.com>
2023-09-11 16:11:31 +0200

    Add files via upload

---
+++ osdwin10.ps1
@@ -0,0 +1,33 @@
+﻿Write-Host  -ForegroundColor Cyan "D2-PC Windows 10 Robot"
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Resolutie naar  1600x"
+    Set-DisRes 1600
+}
+
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Update the awesome D2-OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Cyan "Importing the sweet D2-OSD PowerShell Module"
+Import-Module OSD -Force
+
+#TODO: Spend the time to write a function to do this and put it here
+Write-Host  -ForegroundColor Cyan "Ejecting ISO"
+Write-Warning "That didn't work because I haven't coded it yet!"
+#Start-Sleep -Seconds 5
+
+#Start OSDCloud ZTI the RIGHT way
+Write-Host  -ForegroundColor Cyan "Start OSDCloud with D2 Parameters"
+Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Pro -ZTI
+
+#Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
+Write-Warning "I'm not sure of what to put here yet"
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
